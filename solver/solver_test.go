f26c721ae273bc8a31453958aef2f58a80302276
Zinnatkhanov Petr <zpa@sitek-it.ru>
2020-12-09 23:20:52 +0300

    Begin work on tests for a Solver.solveTask

--- solver/solver_test.go
+++ solver/solver_test.go
@@ -1,76 +1,131 @@
 package solver
 
 import (
-	"reflect"
 	"testing"
 
 	"github.com/Spi1y/tsp-solver/solver/matrix"
+	"github.com/Spi1y/tsp-solver/solver/tasks"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestSolve(t *testing.T) {
-	type args struct {
-		distanceMatrix matrix.Matrix
-	}
+func TestSolver_Solve(t *testing.T) {
 	tests := []struct {
-		name    string
-		args    args
-		want    []int
-		wantErr bool
+		name           string
+		distanceMatrix matrix.Matrix
+		want           []int
+		wantErr        bool
 	}{
 		{
 			"nil",
-			args{
-				nil,
-			},
+			matrix.ConvertToMatrix([][]int{}),
 			nil,
 			true,
 		},
 		{
-			"non square",
-			args{
-				matrix.Matrix{
-					{0, 1},
-					{0, 1},
-					{0, 1},
-				},
-			},
-			nil,
-			true,
-		},
-		{
-			"non square 2",
-			args{
-				matrix.Matrix{
-					{0, 1},
-					{0},
-				},
-			},
-			nil,
-			true,
-		},
-		{
-			"problem - 2",
-			args{
-				matrix.Matrix{
-					{0, 1, 9},
-					{9, 0, 1},
-					{1, 9, 0},
-				},
-			},
+			"Normal - 2 points",
+			matrix.ConvertToMatrix([][]int{
+				{0, 1, 9},
+				{9, 0, 1},
+				{1, 9, 0},
+			}),
 			[]int{0, 1, 2, 0},
 			false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := Solve(tt.args.distanceMatrix)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Solve() error = %v, wantErr %v", err, tt.wantErr)
-				return
+			s := &Solver{}
+			s.DistanceMatrix = tt.distanceMatrix
+			got, err := s.Solve()
+
+			assert.Equal(t, tt.want, got)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Solve() = %v, want %v", got, tt.want)
-			}
+		})
+	}
+}
+
+func TestSolver_solveTask(t *testing.T) {
+	tests := []struct {
+		name         string
+		bestDistance int
+		task         *tasks.Task
+		want         []*tasks.Task
+	}{
+		{
+			"nil",
+			0,
+			nil,
+			nil,
+		},
+		{
+			"First task",
+			0,
+			&tasks.Task{
+				Path:     []int{0},
+				Distance: 4,
+				DistanceMatrix: matrix.ConvertToMatrix([][]int{
+					{-1, 0, 8, 8},
+					{8, 8, 0, 8},
+					{8, 8, 8, 0},
+					{0, 8, 8, 8},
+				}),
+			},
+			[]*tasks.Task{
+				{
+					Path:     []int{0, 1},
+					Distance: 4,
+					DistanceMatrix: [][]int{
+						{-1, -1, -1, -1},
+						{-1, -1, 0, 8},
+						{8, -1, 8, 0},
+						{0, -1, 8, 8},
+					},
+				},
+				{
+					Path:     []int{0, 2},
+					Distance: 20,
+					DistanceMatrix: [][]int{
+						{-1, -1, -1, -1},
+						{0, 0, -1, 0},
+						{-1, 8, -1, 0},
+						{0, 8, -1, 8},
+					},
+				},
+				{
+					Path:     []int{0, 3},
+					Distance: 28,
+					DistanceMatrix: [][]int{
+						{-1, -1, -1, -1},
+						{8, 8, 0, -1},
+						{0, 0, 0, -1},
+						{-1, 0, 0, -1},
+					},
+				},
+			},
+		},
+		// {
+		// 	"Middle task",
+		// 	0,
+		// 	&tasks.Task{},
+		// 	[]*tasks.Task{{}},
+		// },
+		// {
+		// 	"Last node",
+		// 	0,
+		// 	&tasks.Task{},
+		// 	[]*tasks.Task{{}},
+		// },
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Solver{}
+			tasks := s.solveTask(tt.task)
+
+			assert.Equal(t, tt.want, tasks)
 		})
 	}
 }
